28df0bc1615059f9963c35a2a80f5142ed71462c
Muhammad Albie Al Hasan <alhasan99dev@gmail.com>
2019-05-12 12:01:07 +0800

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://alhasandev.github.io/wpu-login/';
+$config['base_url'] = 'https://github.com/alHasandev/wpu-login';
 
 /*
 |--------------------------------------------------------------------------
