6bdac7b75c9eb1f541f7ff3521b1c3d6bd35ebfc
Alhasandev <alhasan99dev@gmail.com>
2019-05-12 13:53:20 +0800

    mengubah foto default menjadi male.png

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -101,7 +101,7 @@ class Auth extends CI_Controller
             $data = [
                 'name' => htmlspecialchars($this->input->post('name', true)),
                 'email' => $email,
-                'image' => 'default.jpg',
+                'image' => 'male.jpg',
                 'password' => password_hash($this->input->post('password1'), PASSWORD_DEFAULT),
                 'role_id' => 2,
                 'is_active' => 0,
@@ -295,13 +295,13 @@ class Auth extends CI_Controller
 
     public function changePassword()
     {
-        if(!$this->session->userdata('reset_email')) {
+        if (!$this->session->userdata('reset_email')) {
             redirect('auth');
         }
         $this->form_validation->set_rules('password1', 'Password', 'trim|required|min_length[3]|matches[password2]');
         $this->form_validation->set_rules('password2', 'Re-Password', 'trim|required|min_length[3]|matches[password1]');
 
-        if($this->form_validation->run() == false) {
+        if ($this->form_validation->run() == false) {
             $data['title'] = 'Change Password';
             $this->load->view('templates/auth_header', $data);
             $this->load->view('auth/change-password');
@@ -320,6 +320,5 @@ class Auth extends CI_Controller
             $this->session->set_flashdata('message', '<div class="alert alert-success" role="alert">Password has been changed! Please login.</div>');
             redirect('auth');
         }
-        
     }
 }
